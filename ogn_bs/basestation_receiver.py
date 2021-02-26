e0c9c206c8e8b9757692332ff3025913c62d0922
s-john1 <78550266+s-john1@users.noreply.github.com>
2021-02-26 03:30:08 +0000

    Re-organise methods

--- ogn_bs/basestation_receiver.py
+++ ogn_bs/basestation_receiver.py
@@ -40,24 +40,7 @@ class BasestationReceiver:
 
     def process_beacon(self, beacon):
         if beacon.get('aprs_type') == 'position' and beacon.get('beacon_type') != 'receiver':
-            send_message = False
-
-            # Find out whether current aircraft object already exists
-            for aircraft in self._aircraft:
-                if aircraft.device_id == beacon.get('name'):
-                    break
-            else:
-                aircraft = None
-
-            if aircraft is None:  # Create new aircraft object
-                self._aircraft.append(Aircraft(beacon.get('name'), beacon.get('timestamp')))
-            else:  # Use existing aircraft object
-                if aircraft.is_not_older_time(beacon.get('timestamp')):  # Use this beacon if its not old
-                    send_message = True
-                    aircraft.time = beacon.get('timestamp')
-                else:  # Beacon older than previous one - don't use to avoid 'jumpy' aircraft trails
-                    send_message = False
-                    self.debug(f'Not using position of {aircraft}')
+            send_message = self._should_send_message(beacon)
 
             basestation = convert_to_basestation(mode_s_hex=beacon.get('name')[3:9],
                                                  altitude=beacon.get('altitude'),
@@ -69,6 +52,38 @@ class BasestationReceiver:
 
             if send_message:
                 self._send_message(basestation)
+
+    def _should_send_message(self, beacon):
+        aircraft = self._find_aircraft(beacon.get('name'))  # Look for existing aircraft object
+
+        if aircraft is None:  # Create new aircraft object
+            aircraft = self._add_aircraft(beacon.get('name'), beacon.get('timestamp'))
+
+        return self._check_message_age(aircraft, beacon.get('timestamp'))
+
+    def _find_aircraft(self, device_id):
+        # Find out whether current aircraft object already exists
+        for aircraft in self._aircraft:
+            if aircraft.device_id == device_id:
+                return aircraft
+        else:
+            return None
+
+    def _add_aircraft(self, device_id, timestamp):
+        aircraft = Aircraft(device_id, timestamp)
+        self._aircraft.append(aircraft)
+
+        return aircraft
+
+    def _check_message_age(self, aircraft, timestamp):
+        if aircraft.is_not_older_time(timestamp):  # Use this beacon if it is not old
+            send_message = True
+            aircraft.time = timestamp  # Update timestamp of aircraft
+        else:  # Beacon older than previous one - don't use to avoid 'jumpy' aircraft trails
+            send_message = False
+            self.debug(f'Not using position of {aircraft} as message is old')
+
+        return send_message
 
     def _send_message(self, basestation):
         self.debug(f'Sending ({self._address}:{self._port}): {basestation}')
