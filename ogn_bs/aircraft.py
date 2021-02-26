387ed03369268f8b610d270db3b90b469b8bc46d
s-john1 <78550266+s-john1@users.noreply.github.com>
2021-02-26 01:55:17 +0000

    Implement Aircraft class to prevent old positions being sent

--- ogn_bs/aircraft.py
+++ ogn_bs/aircraft.py
@@ -1,2 +1,34 @@
+from datetime import datetime
+
+
 class Aircraft:
-    pass
+    _time = None
+
+    def __init__(self, device_id, current_time):
+        self.device_id = device_id
+        self._check_time_instance(current_time)
+        self._time = current_time
+
+    def __repr__(self):
+        return f'Aircraft(device_id={self.device_id})'
+
+    def __str__(self):
+        return f'Aircraft: {self.device_id}'
+
+    @property
+    def time(self):
+        return self._time
+
+    @time.setter
+    def time(self, value):
+        self._check_time_instance(value)
+        self._time = value
+
+    @staticmethod
+    def _check_time_instance(value):
+        if not isinstance(value, datetime):
+            raise TypeError("A datetime object must be used")
+
+    def is_not_older_time(self, time):
+        return time >= self._time
+
--- ogn_bs/basestation_receiver.py
+++ ogn_bs/basestation_receiver.py
@@ -1,7 +1,7 @@
 import socket
 import time
-import sys
 from .basestation_parser import convert_to_basestation
+from .aircraft import Aircraft
 
 
 class BasestationReceiver:
@@ -15,21 +15,40 @@ class BasestationReceiver:
     def connect(self):
         connected = False
         while not connected:
-            print('Attempting to connect to {self._address}:{self._port}'.format(self=self))
+            print(f'Attempting to connect to {self._address}:{self._port}')
             try:
                 self._s.connect((self._address, self._port))
                 connected = True
                 print('Connection successful!\n')
 
             except socket.error as exception:
-                print('Unable to connect to socket: {}\n'.format(exception))
+                print(f'Unable to connect to socket: {exception}\n')
                 time.sleep(5)
 
     def disconnect(self):
         self._s.close()
 
     def process_beacon(self, beacon):
-        if beacon.get('aprs_type') == 'position':
+        if beacon.get('aprs_type') == 'position' and beacon.get('beacon_type') != 'receiver':
+            send_message = False
+
+            # Find out whether current aircraft object already exists
+            for aircraft in self._aircraft:
+                if aircraft.device_id == beacon.get('name'):
+                    break
+            else:
+                aircraft = None
+
+            if aircraft is None:  # Create new aircraft object
+                self._aircraft.append(Aircraft(beacon.get('name'), beacon.get('timestamp')))
+            else:  # Use existing aircraft object
+                if aircraft.is_not_older_time(beacon.get('timestamp')):  # Use this beacon if its not old
+                    send_message = True
+                    aircraft.time = beacon.get('timestamp')
+                else:  # Beacon older than previous one - don't use to avoid 'jumpy' aircraft trails
+                    send_message = False
+                    print('Not using position of', aircraft)
+
             basestation = convert_to_basestation(mode_s_hex=beacon.get('name')[3:9],
                                                  altitude=beacon.get('altitude'),
                                                  ground_speed=beacon.get('ground_speed'),
@@ -38,4 +57,10 @@ class BasestationReceiver:
                                                  longitude=beacon.get('longitude'),
                                                  vertical_rate=beacon.get('climb_rate'))
 
-            self._s.send((basestation + "\n").encode())
+            if send_message:
+                self._send_message(basestation)
+
+    def _send_message(self, basestation):
+        print(f'Sending ({self._address}:{self._port}): {basestation}')
+        self._s.send((basestation + "\n").encode())
+
