ef8185d0885e10d8c56d481a67e1c32cf9f1785d
s-john1 <78550266+s-john1@users.noreply.github.com>
2022-04-10 16:48:43 +0100

    Add additional Aircraft attributes

--- ogn_bs/aircraft.py
+++ ogn_bs/aircraft.py
@@ -1,8 +1,10 @@
+import re
 from datetime import datetime
 
 
 class Aircraft:
-    _time = None
+    _icao = None
+    _registration = None
 
     def __init__(self, device_id, current_time):
         self.device_id = device_id
@@ -10,10 +12,11 @@ class Aircraft:
         self._time = current_time
 
     def __repr__(self):
-        return f'Aircraft(device_id={self.device_id}, time={repr(self._time)})'
+        return f'Aircraft(device_id={self.device_id}, time={repr(self._time)}, icao={self._icao},' \
+               f'registration={self._registration})'
 
     def __str__(self):
-        return f'Aircraft: {self.device_id}, Time: {self._time}'
+        return f'Aircraft: {self.device_id}, Time: {self._time}, ICAO: {self._icao}, Registration: {self._registration}'
 
     @property
     def time(self):
@@ -23,6 +26,25 @@ class Aircraft:
     def time(self, value):
         self._check_time_instance(value)
         self._time = value
+
+    @property
+    def icao(self):
+        return self._icao
+
+    @icao.setter
+    def icao(self, value):
+        if not re.match("^([A-Fa-f0-9]{6})$", value):
+            raise TypeError("ICAO is not in 6 digit hex format")
+
+        self._icao = value
+
+    @property
+    def registration(self):
+        return self._registration
+
+    @registration.setter
+    def registration(self, value):
+        self._registration = value
 
     @staticmethod
     def _check_time_instance(value):
